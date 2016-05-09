089890a3e05a52f4bad2bfa058783263003810b1
hecforga <kle-tor@hotmail.com>
2016-05-09 16:31:09 +0200

    .gitignore is now working

--- app/adapters/jsonforms.adapter.js
+++
@@ -1,3 +0,0 @@
-var upgrade_adapter_1 = require('./upgrade_adapter');
-exports.JsonFormsAdapter = upgrade_adapter_1.upgradeAdapter.upgradeNg1Component('jsonforms');
-//# sourceMappingURL=jsonforms.adapter.js.map
