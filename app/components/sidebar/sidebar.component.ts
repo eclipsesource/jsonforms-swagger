2754bfb92cdb83c5227ce36d220306eda4c9ff59
pancho111203 <pancho_111203@hotmail.com>
2016-05-17 12:16:55 +0200

    Added description box and title of api, ui changes

--- app/components/sidebar/sidebar.component.ts
+++ app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { IObserver } from '../../helpers/observer/observer.interface';
 
@@ -12,17 +12,22 @@ import { Operation } from '../core/model/operation';
 import { JsonFormsAdapter } from '../../adapters/jsonforms.adapter';
 
 import { PanelMenu } from 'primeng/primeng';
-
+import {OverlayPanel} from 'primeng/primeng';
 
 
 @Component({
   selector: 'sidebar',
   moduleId: module.id,
   templateUrl: 'sidebar.html',
-  directives: [JsonFormsAdapter, PanelMenu],
+  directives: [JsonFormsAdapter, PanelMenu, OverlayPanel],
   styleUrls: ['sidebar.css'],
 })
 export class SidebarComponent implements IObserver {
+
+  @ViewChild('op') op;
+
+  moreInfoActive: boolean = false;
+  moreInfoTimeoutId: number = null;
 
   devMode: boolean = false;
 
@@ -77,6 +82,50 @@ export class SidebarComponent implements IObserver {
 
   onClickOperation(operation: Operation) {
     this.activeOperationService.setActiveOperation(operation, {});
+  }
+
+  getTitle(): string{
+    if(this.api && this.api.properties && this.api.properties['info'] && this.api.properties['info']['title']){
+      return this.api.properties['info']['title'];
+    }else {
+      return null;
+    }
+  }
+
+  getDescription(): string {
+    if(this.api && this.api.properties && this.api.properties['info'] && this.api.properties['info']['description']){
+      return this.api.properties['info']['description'];
+    }else {
+      return null;
+    }
+  }
+
+  descriptionHoverIn($event, infoTarget){
+
+    console.log('in');
+
+    if(this.moreInfoActive){
+      console.log('reenter');
+      console.log(this.moreInfoTimeoutId);
+      if(this.moreInfoTimeoutId){
+
+        clearTimeout(this.moreInfoTimeoutId);
+        this.moreInfoTimeoutId = null;
+      }
+    }else {
+      this.op.show($event, infoTarget);
+      this.moreInfoActive = true;
+    }
+  }
+  descriptionHoverOut($event){
+    console.log('out');
+
+    this.moreInfoTimeoutId = setTimeout(()=>{
+      this.op.hide($event);
+      this.moreInfoTimeoutId = null;
+      this.moreInfoActive = false;
+
+    }, 1000);
   }
 
 }
